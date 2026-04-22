a504973a1d748833076b15478a1458b5b7edf0d7
Solomon Kaluma Mcharo <solomonkalumamcharo@Solomons-MacBook-Pro-3.local>
2026-04-22 16:31:58 +0300

    Adding Image Slider

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function toggleText(box) {
   box.classList.toggle("active");
 }
+
+const slides = document.getElementById("slides");
+const totalSlides = slides.children.length;
+let index = 0;
+
+function autoSlide() {
+  index++;
+  if (index >= totalSlides) {
+    index = 0;
+  }
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(autoSlide, 3000);
