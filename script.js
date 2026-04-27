64f1493692cca768b65a5104f0e1d7ebcb1f56bc
Solomon Kaluma Mcharo <solomonkalumamcharo@Solomons-MacBook-Pro-3.local>
2026-04-27 12:22:26 +0300

    Slides Update

--- script.js
+++ script.js
@@ -14,7 +14,19 @@ function autoSlide() {
   slides.style.transform = `translateX(-${index * 100}%)`;
 }
 
-setInterval(autoSlide, 3000);
+let slideInterval = setInterval(autoSlide, 3000);
+
+let isPaused = false;
+
+slides.addEventListener("click", () => {
+  if (!isPaused) {
+    clearInterval(slideInterval);
+    isPaused = true;
+  } else {
+    slideInterval = setInterval(autoSlide, 3000);
+    isPaused = false;
+  }
+});
 
 
 const slides2 = document.getElementById("slides2");
@@ -28,8 +40,20 @@ function autoSlide2() {
   }
   slides2.style.transform = `translateX(-${index2 * 100}%)`;
 }
-setInterval(autoSlide2, 3000);
 
+let slideInterval2 = setInterval(autoSlide2, 3000);
+
+let isPaused2 = false;
+
+slides2.addEventListener("click", () => {
+  if (!isPaused2) {
+    clearInterval(slideInterval2);
+    isPaused2 = true;
+  } else {
+    slideInterval2 = setInterval(autoSlide2, 3000);
+    isPaused2 = false;
+  }
+});
 
 
 const slides3 = document.getElementById("slides3");
@@ -43,7 +67,19 @@ function autoSlide3() {
   }
   slides3.style.transform = `translateX(-${index3 * 100}%)`;
 }
-setInterval(autoSlide3, 3000);
+let slideInterval3 = setInterval(autoSlide3, 3000);
+
+let isPaused3 = false;
+
+slides3.addEventListener("click", () => {
+  if (!isPaused3) {
+    clearInterval(slideInterval3);
+    isPaused3 = true;
+  } else {
+    slideInterval3 = setInterval(autoSlide3, 3000);
+    isPaused3 = false;
+  }
+});
 
 window.addEventListener("load", () => {
   const banner = document.getElementById("banner");
@@ -69,7 +105,20 @@ function autoSlide4() {
   }
   slides4.style.transform = `translateX(-${index4 * 100}%)`;
 }
-setInterval(autoSlide4, 3000);
+
+let slideInterval4 = setInterval(autoSlide4, 3000);
+
+let isPaused4 = false;
+
+slides4.addEventListener("click", () => {
+  if (!isPaused4) {
+    clearInterval(slideInterval4);
+    isPaused4 = true;
+  } else {
+    slideInterval4 = setInterval(autoSlide4, 3000);
+    isPaused4 = false;
+  }
+});
 
 
 const slides5 = document.getElementById("slides5");
@@ -83,4 +132,16 @@ function autoSlide5() {
   }
   slides5.style.transform = `translateX(-${index5 * 100}%)`;
 }
-setInterval(autoSlide5, 3000);
+let slideInterval5 = setInterval(autoSlide5, 3000);
+
+let isPaused5 = false;
+
+slides5.addEventListener("click", () => {
+  if (!isPaused5) {
+    clearInterval(slideInterval5);
+    isPaused5 = true;
+  } else {
+    slideInterval5 = setInterval(autoSlide5, 3000);
+    isPaused5 = false;
+  }
+});
