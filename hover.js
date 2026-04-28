83090cfce719d8e16dd99fdfcf76f570bd67cb7c
Solomon Kaluma Mcharo <solomonkalumamcharo@Solomons-MacBook-Pro-3.local>
2026-04-28 14:29:15 +0300

    Contrast Theme 0.1

--- hover.js
+++ hover.js
@@ -194,7 +194,7 @@ function setCssVars({ base01, baseText, hover01, hoverText, pageBg01 }){
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  applyFromInput("255, 255, 255");
+  applyFromInput(0);
 });
 
 
